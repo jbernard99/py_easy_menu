f00747c621e43c3f4f5d3ee5f7348658f71deac5
jbernard99 <julien2499@hotmail.fr>
2022-01-12 10:08:21 -0500

    -Added main_test to launch basic menu\n-

---
+++ main_test.py
@@ -0,0 +1,7 @@
+import menu
+
+menu = menu.Menu()
+
+choices = ['one', 'two', 'three', 'four']
+menu.clear()
+menu.print_menu('Choose one of these:', choice_list=choices)
--- menu.py
+++ menu.py
@@ -1,31 +1,36 @@
-from os import system
+from os import system, name
 
 class Menu:
 
 	def __init__(self):
-		self.PURPLE = '\033[95m'
-		self.LIGHT_BLUE = '\033[94m'
-		self.CYAN = '\033[96m'
-		self.GREEN = '\033[92m'
-		self.ORANGE = '\033[93m'
-		self.RED = '\033[91m'
-		self.ENDC = '\033[0m'
-		self.BOLD = '\033[1m'
-		self.UNDERLINE = '\033[4m'
+		self.colors = {
+			'purple': '\033[95m',
+			'light_blue': '\033[94m',
+			'cyan': '\033[96m',
+			'green': '\033[92m',
+			'orange': '\033[93m',
+			'red': '\033[91m',
+			'endc': '\033[0m',
+			'bold': '\033[1m',
+			'unederline': '\033[4m'
+		}
 
 	def clear(self):
-		system("clear")
+		 if name == 'nt':
+		 	system('cls')
+		 else:
+		 	system('clear')
 
 	def print_error(self, error):
 		self.clear()
-		print(self.BOLD + self.RED + "ERROR: " + self.ENDC + error)
+		print(self.colors['bold'] + self.colors['red'] + "ERROR: " + self.colors['endc'] + error)
 
 	def print_menu(self, txt, choice_list=["\n"]):
 		self.clear()
-		print(self.CYAN + txt + "\n\n")
+		print(self.colors['cyan'] + txt + "\n\n")
 		i = 1
 		for choice in choice_list:
-			print(self.CYAN + str(i) + " - " + self.GREEN + choice)
+			print(self.colors['cyan'] + str(i) + " - " + self.colors['green'] + choice)
 			i += 1
 		print("\n\n")
 		choice = self.get_input("int", end=len(choice_list))
@@ -51,9 +56,9 @@ class Menu:
 
 	def print_result(self, result_title, result_list):
 		self.clear()
-		print(self.CYAN + "Results for" + self.BOLD + result_title + "\n")
+		print(self.colors['cyan'] + "Results for" + self.colors['bold'] + result_title + "\n")
 		i = 1
 		for result in result_list:
-			print(self.CYAN + str(i) + " - " + self.GREEN + str(result))
+			print(self.colors['cyan'] + str(i) + " - " + self.colors['green'] + str(result))
 			i += 1
 		input("Press enter to continue...")
